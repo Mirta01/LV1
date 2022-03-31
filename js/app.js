225274a6bc528818499e487e3423c0233de6249d
Mirta <mirta.domovic@vuv.hr>
2022-03-31 16:48:51 +0200

    Treci commit

--- js/app.js
+++ js/app.js
@@ -6,14 +6,13 @@ function Klik(){
     var inp1 = $("#input1").val();
     var inp2 = $("#input2").val();
     var inp3 = $("#input3").val();
-    var btn = $("<button class='btn-lite btnObrisi'>Obriši</button>")
-    
     
     $(tr).appendTo(tbody);
     $("<td></td>").appendTo(tr).text(inp1);
     $("<td></td>").appendTo(tr).text(inp2);
     $("<td></td>").appendTo(tr).text(inp3);
     $("<td class='middle'><button class='btnObrisi' onclick='Obrisi()'>Obriši</button></td>").appendTo(tr);
+    $("<td class='middle'><input type='checkbox' id='predmet' onclick='PolozenP()'></td>").appendTo(tr);
 }
 
 function Obrisi(){
@@ -23,3 +22,12 @@ function Obrisi(){
 	
 }
 
+function PolozenP()
+{
+    var check = event.target;
+	var td = event.target.parentNode;
+	if (check.checked == true)
+	{
+		td.parentNode.setAttribute("style","background-color:rgb(221, 163, 208);");
+	}
+}
